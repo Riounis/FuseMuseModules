a0f198065f4b9c0404b21731a8c48a996d843ec5
sjrappl <sjrappl@ncsu.edu>
2018-05-13 22:32:19 -0400

    Changed driver module
    To use new _ style instead of camelcase, to match the library.

--- Driver Modules/Verse-Chorus Driver Module/versechorusdm.cpp
+++ Driver Modules/Verse-Chorus Driver Module/versechorusdm.cpp
@@ -41,21 +41,21 @@ int main() {
 	int time = rand() % 100;
 	if (time < 75) {
 		TimeSignature timeSig(4, 4);
-		mets->timeSignature = timeSig;
+		mets->time_signature = timeSig;
 	}
 	else if (time < 95) {
 		TimeSignature timeSig(3, 4);
-		mets->timeSignature = timeSig;
+		mets->time_signature = timeSig;
 	}
 	else {
 		TimeSignature timeSig(6, 8);
-		mets->timeSignature = timeSig;
+		mets->time_signature = timeSig;
 	}
 	int temp = 60;
 	temp += rand() % 180;
 	mets->tempo = temp;
 	mets->position = 0;
-	comp.setInitialCompositionMetrics(mets);
+	comp.set_initial_composition_metrics(&mets);
 	//printf("metrics generated\n");
 	
 	// Generate generic chord progression
@@ -124,19 +124,19 @@ int main() {
 	TimeSignature introTimeSig;
 	int introTempo;*/
 	int introLength = (rand() % 2 + 1) * 4;
-	PatternSegment *intro = new PatternSegment("intro", introLength * mets->timeSignature.durationOfMeasure());
+	PatternSegment *intro = new PatternSegment("intro", introLength * mets->time_signature.duration_of_measure());
 	Part *introPart = new Part();
 	for (int i = 0; i < introLength / 4; i++) {
-		Chord *c = new Chord(mets->key.getSimpleChord(chords[0]), mets->timeSignature.durationOfMeasure());
-		Chord *c1 = new Chord(mets->key.getSimpleChord(chords[1]), mets->timeSignature.durationOfMeasure());
-		Chord *c2 = new Chord(mets->key.getSimpleChord(chords[2]), mets->timeSignature.durationOfMeasure());
-		Chord *c3 = new Chord(mets->key.getSimpleChord(chords[3]), mets->timeSignature.durationOfMeasure());
-		introPart->appendChord(c);
-		introPart->appendChord(c1);
-		introPart->appendChord(c2);
-		introPart->appendChord(c3);
+		Chord *c = new Chord(mets->key.get_simple_chord(chords[0]), mets->time_signature.duration_of_measure());
+		Chord *c1 = new Chord(mets->key.get_simple_chord(chords[1]), mets->time_signature.duration_of_measure());
+		Chord *c2 = new Chord(mets->key.get_simple_chord(chords[2]), mets->time_signature.duration_of_measure());
+		Chord *c3 = new Chord(mets->key.get_simple_chord(chords[3]), mets->time_signature.duration_of_measure());
+		introPart->append_chord(c);
+		introPart->append_chord(c1);
+		introPart->append_chord(c2);
+		introPart->append_chord(c3);
 	}
-	intro->setChordProgression(introPart);
+	intro->set_chord_progression(introPart);
 	//printf("intro generated\n");
 	
 	// Generate Verse: can be 8, 12, 16, or 20 measures
@@ -145,19 +145,19 @@ int main() {
 	TimeSignature verseTimeSig;
 	int verseTempo;*/
 	int verseLength = (rand() % 4 + 2) * 4;
-	PatternSegment *verse = new PatternSegment("verse", verseLength * mets->timeSignature.durationOfMeasure());
+	PatternSegment *verse = new PatternSegment("verse", verseLength * mets->time_signature.duration_of_measure());
 	Part *versePart = new Part();
 	for (int i = 0; i < verseLength / 4; i++) {
-		Chord *c = new Chord(mets->key.getSimpleChord(chords[0]), mets->timeSignature.durationOfMeasure());
-		Chord *c1 = new Chord(mets->key.getSimpleChord(chords[1]), mets->timeSignature.durationOfMeasure());
-		Chord *c2 = new Chord(mets->key.getSimpleChord(chords[2]), mets->timeSignature.durationOfMeasure());
-		Chord *c3 = new Chord(mets->key.getSimpleChord(chords[3]), mets->timeSignature.durationOfMeasure());
-		versePart->appendChord(c);
-		versePart->appendChord(c1);
-		versePart->appendChord(c2);
-		versePart->appendChord(c3);
+		Chord *c = new Chord(mets->key.get_simple_chord(chords[0]), mets->time_signature.duration_of_measure());
+		Chord *c1 = new Chord(mets->key.get_simple_chord(chords[1]), mets->time_signature.duration_of_measure());
+		Chord *c2 = new Chord(mets->key.get_simple_chord(chords[2]), mets->time_signature.duration_of_measure());
+		Chord *c3 = new Chord(mets->key.get_simple_chord(chords[3]), mets->time_signature.duration_of_measure());
+		versePart->append_chord(c);
+		versePart->append_chord(c1);
+		versePart->append_chord(c2);
+		versePart->append_chord(c3);
 	}
-	verse->setChordProgression(versePart);
+	verse->set_chord_progression(versePart);
 	//printf("verse generated\n");
 	
 	// Generate Pre-Chorus: can be 4 measures
@@ -166,19 +166,19 @@ int main() {
 	TimeSignature prechorusTimeSig;
 	int prechorusTempo;*/
 	int prechorusLength = 4;
-	PatternSegment* prechorus = new PatternSegment("prechorus", prechorusLength * mets->timeSignature.durationOfMeasure());
+	PatternSegment* prechorus = new PatternSegment("prechorus", prechorusLength * mets->time_signature.duration_of_measure());
 	Part *prechorusPart = new Part();
 	for (int i = 0; i < prechorusLength / 4; i++) {
-		Chord *c = new Chord(mets->key.getSimpleChord(chords[0]), mets->timeSignature.durationOfMeasure());
-		Chord *c1 = new Chord(mets->key.getSimpleChord(chords[1]), mets->timeSignature.durationOfMeasure());
-		Chord *c2 = new Chord(mets->key.getSimpleChord(chords[2]), mets->timeSignature.durationOfMeasure());
-		Chord *c3 = new Chord(mets->key.getSimpleChord(chords[3]), mets->timeSignature.durationOfMeasure());
-		prechorusPart->appendChord(c);
-		prechorusPart->appendChord(c1);
-		prechorusPart->appendChord(c2);
-		prechorusPart->appendChord(c3);
+		Chord *c = new Chord(mets->key.get_simple_chord(chords[0]), mets->time_signature.duration_of_measure());
+		Chord *c1 = new Chord(mets->key.get_simple_chord(chords[1]), mets->time_signature.duration_of_measure());
+		Chord *c2 = new Chord(mets->key.get_simple_chord(chords[2]), mets->time_signature.duration_of_measure());
+		Chord *c3 = new Chord(mets->key.get_simple_chord(chords[3]), mets->time_signature.duration_of_measure());
+		prechorusPart->append_chord(c);
+		prechorusPart->append_chord(c1);
+		prechorusPart->append_chord(c2);
+		prechorusPart->append_chord(c3);
 	}
-	prechorus->setChordProgression(prechorusPart);
+	prechorus->set_chord_progression(prechorusPart);
 	//printf("prechorus generated\n");
 	
 	// Generate Chorus: can be 4, 8, or 16 measures
@@ -187,19 +187,19 @@ int main() {
 	TimeSignature chorusTimeSig;
 	int chorusTempo;*/
 	int chorusLength = pow(2, rand() % 3 + 2);
-	PatternSegment *chorus = new PatternSegment("chorus", chorusLength * mets->timeSignature.durationOfMeasure());
+	PatternSegment *chorus = new PatternSegment("chorus", chorusLength * mets->time_signature.duration_of_measure());
 	Part *chorusPart = new Part();
 	for (int i = 0; i < chorusLength / 4; i++) {
-		Chord *c = new Chord(mets->key.getSimpleChord(chords[0]), mets->timeSignature.durationOfMeasure());
-		Chord *c1 = new Chord(mets->key.getSimpleChord(chords[1]), mets->timeSignature.durationOfMeasure());
-		Chord *c2 = new Chord(mets->key.getSimpleChord(chords[2]), mets->timeSignature.durationOfMeasure());
-		Chord *c3 = new Chord(mets->key.getSimpleChord(chords[3]), mets->timeSignature.durationOfMeasure());
-		chorusPart->appendChord(c);
-		chorusPart->appendChord(c1);
-		chorusPart->appendChord(c2);
-		chorusPart->appendChord(c3);
+		Chord *c = new Chord(mets->key.get_simple_chord(chords[0]), mets->time_signature.duration_of_measure());
+		Chord *c1 = new Chord(mets->key.get_simple_chord(chords[1]), mets->time_signature.duration_of_measure());
+		Chord *c2 = new Chord(mets->key.get_simple_chord(chords[2]), mets->time_signature.duration_of_measure());
+		Chord *c3 = new Chord(mets->key.get_simple_chord(chords[3]), mets->time_signature.duration_of_measure());
+		chorusPart->append_chord(c);
+		chorusPart->append_chord(c1);
+		chorusPart->append_chord(c2);
+		chorusPart->append_chord(c3);
 	}
-	chorus->setChordProgression(chorusPart);
+	chorus->set_chord_progression(chorusPart);
 	//printf("chorus generated\n");
 	
 	// Generate Bridge: can be 8 or 16 measures
@@ -208,91 +208,91 @@ int main() {
 	TimeSignature bridgeTimeSig;
 	int bridgeTempo;*/
 	int bridgeLength = (rand() % 2 + 1) * 8;
-	PatternSegment *bridge = new PatternSegment("bridge", bridgeLength * mets->timeSignature.durationOfMeasure());
+	PatternSegment *bridge = new PatternSegment("bridge", bridgeLength * mets->time_signature.duration_of_measure());
 	Part *bridgePart = new Part;
 	for (int i = 0; i < bridgeLength / 4; i++) {
-		Chord *c = new Chord(mets->key.getSimpleChord(chords[0]), mets->timeSignature.durationOfMeasure());
-		Chord *c1 = new Chord(mets->key.getSimpleChord(chords[1]), mets->timeSignature.durationOfMeasure());
-		Chord *c2 = new Chord(mets->key.getSimpleChord(chords[2]), mets->timeSignature.durationOfMeasure());
-		Chord *c3 = new Chord(mets->key.getSimpleChord(chords[3]), mets->timeSignature.durationOfMeasure());
-		bridgePart->appendChord(c);
-		bridgePart->appendChord(c1);
-		bridgePart->appendChord(c2);
-		bridgePart->appendChord(c3);
+		Chord *c = new Chord(mets->key.get_simple_chord(chords[0]), mets->time_signature.duration_of_measure());
+		Chord *c1 = new Chord(mets->key.get_simple_chord(chords[1]), mets->time_signature.duration_of_measure());
+		Chord *c2 = new Chord(mets->key.get_simple_chord(chords[2]), mets->time_signature.duration_of_measure());
+		Chord *c3 = new Chord(mets->key.get_simple_chord(chords[3]), mets->time_signature.duration_of_measure());
+		bridgePart->append_chord(c);
+		bridgePart->append_chord(c1);
+		bridgePart->append_chord(c2);
+		bridgePart->append_chord(c3);
 	}
-	bridge->setChordProgression(bridgePart);
+	bridge->set_chord_progression(bridgePart);
 	//printf("bridge generated\n");
 	
 	// Register pattern segments
-	comp.registerPatternSegment(intro);
-	comp.registerPatternSegment(verse);
-	comp.registerPatternSegment(prechorus);
-	comp.registerPatternSegment(chorus);
-	comp.registerPatternSegment(bridge);
+	comp.register_pattern_segment(intro);
+	comp.register_pattern_segment(verse);
+	comp.register_pattern_segment(prechorus);
+	comp.register_pattern_segment(chorus);
+	comp.register_pattern_segment(bridge);
 	//printf("pattern segments registered\n");
 	
 	// generate pattern
 	if (rand() % 2 == 0) {
-		comp.addToPattern("intro");
+		comp.add_to_pattern("intro");
 	}
 	int pchorus = rand() % 2;
 	int pat = rand() % 5;
 	if (pat == 0) {
-		comp.addToPattern("verse");
+		comp.add_to_pattern("verse");
 		if (pchorus == 0) {
-			comp.addToPattern("prechorus");
+			comp.add_to_pattern("prechorus");
 		}
-		comp.addToPattern("chorus");
-		comp.addToPattern("verse");
+		comp.add_to_pattern("chorus");
+		comp.add_to_pattern("verse");
 		if (pchorus == 0) {
-			comp.addToPattern("prechorus");
+			comp.add_to_pattern("prechorus");
 		}
-		comp.addToPattern("chorus");
-		comp.addToPattern("bridge");
-		comp.addToPattern("chorus");
+		comp.add_to_pattern("chorus");
+		comp.add_to_pattern("bridge");
+		comp.add_to_pattern("chorus");
 	}
 	else if (pat == 1) {
-		comp.addToPattern("verse");
-		comp.addToPattern("verse");
-		comp.addToPattern("bridge");
-		comp.addToPattern("verse");
+		comp.add_to_pattern("verse");
+		comp.add_to_pattern("verse");
+		comp.add_to_pattern("bridge");
+		comp.add_to_pattern("verse");
 	}
 	else if (pat == 2) {
-		comp.addToPattern("verse");
+		comp.add_to_pattern("verse");
 		if (pchorus == 0) { 
-			comp.addToPattern("prechorus");
+			comp.add_to_pattern("prechorus");
 		}
-		comp.addToPattern("chorus");
-		comp.addToPattern("verse");
+		comp.add_to_pattern("chorus");
+		comp.add_to_pattern("verse");
 		if (pchorus == 0) { 
-			comp.addToPattern("prechorus");
+			comp.add_to_pattern("prechorus");
 		}
-		comp.addToPattern("chorus");
-		comp.addToPattern("verse");
-		comp.addToPattern("chorus");
+		comp.add_to_pattern("chorus");
+		comp.add_to_pattern("verse");
+		comp.add_to_pattern("chorus");
 	}
 	else if (pat == 3) {
-		comp.addToPattern("verse");
-		comp.addToPattern("verse");
+		comp.add_to_pattern("verse");
+		comp.add_to_pattern("verse");
 		if (pchorus == 0) { 
-			comp.addToPattern("prechorus");
+			comp.add_to_pattern("prechorus");
 		}
-		comp.addToPattern("chorus");
-		comp.addToPattern("verse");
+		comp.add_to_pattern("chorus");
+		comp.add_to_pattern("verse");
 		if (pchorus == 0) { 
-			comp.addToPattern("prechorus");
+			comp.add_to_pattern("prechorus");
 		}
-		comp.addToPattern("chorus");
+		comp.add_to_pattern("chorus");
 	}
 	else {
-		comp.addToPattern("verse");
-		comp.addToPattern("verse");
+		comp.add_to_pattern("verse");
+		comp.add_to_pattern("verse");
 		if (pchorus == 0) { 
-			comp.addToPattern("prechorus");
+			comp.add_to_pattern("prechorus");
 		}
-		comp.addToPattern("chorus");
-		comp.addToPattern("bridge");
-		comp.addToPattern("chorus");
+		comp.add_to_pattern("chorus");
+		comp.add_to_pattern("bridge");
+		comp.add_to_pattern("chorus");
 	}
 	//printf("patterns generated\n");
 	
